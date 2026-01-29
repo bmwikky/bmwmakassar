9fbeb3b9f255e7f8304944eaa31c9dcd8747b735
bmwikky <bmwikky@gmail.com>
2026-01-29 14:25:40 +0800

    Update cars-data.js

--- js/cars-data.js
+++ js/cars-data.js
@@ -36,14 +36,14 @@ const cars = [
     name: "430i Coupé",
     series: "Seri 4",
     price: "Rp. 1.778.000.000",
-    image: "images/cars/430i-coupe.jpg"
+    image: "images/cars/430i-coupe.png"
   },
   {
     id: "430i-convertible",
     name: "430i Convertible",
     series: "Seri 4",
     price: "Rp. 1.941.000.000",
-    image: "images/cars/430i-convertible.jpg"
+    image: "images/cars/430i-convertible.png"
   },
 
   // SERI 5 & 7
@@ -52,14 +52,14 @@ const cars = [
     name: "520i M Sport",
     series: "Seri 5",
     price: "Rp. 1.513.000.000",
-    image: "images/cars/520i.jpg"
+    image: "images/cars/520i.png"
   },
   {
     id: "735i",
     name: "735i M Sport",
     series: "Seri 7",
     price: "Rp. 2.724.000.000",
-    image: "images/cars/735i.jpg"
+    image: "images/cars/735i.png"
   },
 
   // X SERIES
@@ -68,42 +68,42 @@ const cars = [
     name: "X1 sDrive18i xLine",
     series: "The X1",
     price: "Rp. 1.027.000.000",
-    image: "images/cars/x1.jpg"
+    image: "images/cars/x1.png"
   },
   {
     id: "x3",
     name: "X3 20 xDrive M Sport",
     series: "The X3",
     price: "Rp. 1.574.000.000",
-    image: "images/cars/x3.jpg"
+    image: "images/cars/x3.png"
   },
   {
     id: "x5-lci",
     name: "X5 xDrive40i M Sport LCI",
     series: "The X5",
     price: "Rp. 2.058.000.000",
-    image: "images/cars/x5-lci.jpg"
+    image: "images/cars/x5.png"
   },
   {
     id: "x5-pro",
     name: "X5 xDrive40i M Sport Pro",
     series: "The X5",
     price: "Rp. 2.078.000.000",
-    image: "images/cars/x5-pro.jpg"
+    image: "images/cars/x5-pro.png"
   },
   {
     id: "x6",
     name: "X6 xDrive40i M Sport Pro",
     series: "The X6",
     price: "Rp. 2.466.000.000",
-    image: "images/cars/x6.jpg"
+    image: "images/cars/x6.png"
   },
   {
     id: "x7",
     name: "X7 xDrive40i M Sport",
     series: "The X7",
     price: "Rp. 2.875.000.000",
-    image: "images/cars/x7.jpg"
+    image: "images/cars/x7.png"
   },
 
   // Z
@@ -112,14 +112,14 @@ const cars = [
     name: "Z4 sDrive30i M Sport",
     series: "BMW Z",
     price: "Rp. 1.850.000.000",
-    image: "images/cars/z4.jpg"
+    image: "images/cars/z4.png"
   },
   {
     id: "z4-m40i",
     name: "Z4 M40i",
     series: "BMW Z",
     price: "Rp. 2.104.000.000",
-    image: "images/cars/z4-m40i.jpg"
+    image: "images/cars/z4-m40i.png"
   },
 
   // BMW i
@@ -128,14 +128,14 @@ const cars = [
     name: "iX xDrive45 M Sport",
     series: "BMW i",
     price: "Rp. 2.639.000.000",
-    image: "images/cars/ix.jpg"
+    image: "images/cars/ix.png"
   },
   {
     id: "i7",
     name: "i7 xDrive60 Gran Lusso",
     series: "BMW i",
     price: "Rp. 3.429.000.000",
-    image: "images/cars/i7.jpg"
+    image: "images/cars/i7.png"
   },
 
   // BMW M
@@ -144,34 +144,34 @@ const cars = [
     name: "M3 Competition M xDrive",
     series: "BMW M",
     price: "Rp. 2.880.000.000",
-    image: "images/cars/m3.jpg"
+    image: "images/cars/m3.png"
   },
   {
     id: "m3-cs",
     name: "M3 CS Touring",
     series: "BMW M",
     price: "Rp. 2.866.000.000",
-    image: "images/cars/m3-cs.jpg"
+    image: "images/cars/m3-cs.png"
   },
   {
     id: "m4-coupe",
     name: "M4 Competition Coupé M xDrive",
     series: "BMW M",
     price: "Rp. 2.875.000.000",
-    image: "images/cars/m4-coupe.jpg"
+    image: "images/cars/m4-coupe.png"
   },
   {
     id: "m4-convertible",
     name: "M4 Competition Convertible M xDrive",
     series: "BMW M",
     price: "Rp. 3.263.000.000",
-    image: "images/cars/m4-convertible.jpg"
+    image: "images/cars/m4.png"
   },
   {
     id: "m4-cs",
     name: "M4 CS",
     series: "BMW M",
     price: "Rp. 3.560.000.000",
-    image: "images/cars/m4-cs.jpg"
+    image: "images/cars/m4-cs.png"
   }
 ];
